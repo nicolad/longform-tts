221f14cdec6faaa7fd05f9139451e0235099f547
Vadim Nicolai <vadim.nicolai@vitrifi.net>
2025-03-17 21:21:47 +0200

    Small improvement.

--- shuttle_prebuild.sh
+++ shuttle_prebuild.sh
@@ -3,12 +3,10 @@
 apt update && apt install -y nodejs
 
 echo "Installing pnpm..."
-wget -qO- https://get.pnpm.io/install.sh | sh -
+wget -qO- https://get.pnpm.io/install.sh | ENV="$HOME/.shrc" SHELL="$(which sh)" sh -
 
-echo "Installing dependencies (frozen lockfile)..."
-pnpm install --frozen-lockfile
+echo "Installing dependencies..."
+pnpm install
 
-echo "Building the project..."
-pnpm build
-
-echo "Done!"
+echo "Building..."
+pnpm run build
